c73a7773cab2934b9aa6ac6bd45e1bc14e29c837
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2021-08-26 19:19:15 -0300

    Inicial cammit

---
+++ exercFixExecao/src/aplicacoes/Programa.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package aplicacoes;
+
+/**
+ * @author leo_dias
+ *
+ */
+public class Programa {
+
+	public static void main(String[] args) {
+		System.out.println("Olá git!");
+		System.out.println("Será que vai?");
+
+	}
+
+}
