2b3303c060e34b7def484d098a0f13e65c9a9f04
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2021-08-26 19:42:52 -0300

    criando classe juridica

--- exercFixExecao/src/entidades/Juridica.java
+++ exercFixExecao/src/entidades/Juridica.java
@@ -7,7 +7,38 @@ package entidades;
  * @author leo_dias
  *
  */
-public class Juridica {
+public class Juridica extends Pessoa {
+	
+	private int numeroFuncionarios;
+
+	public Juridica() {
+		super();
+	}
+
+	public Juridica(String nome, double rendaAnual, int numeroFuncionarios) {
+		super(nome, rendaAnual);
+		this.numeroFuncionarios = numeroFuncionarios;
+	}
+
+	public int getNumeroFuncionarios() {
+		return numeroFuncionarios;
+	}
+
+	public void setNumeroFuncionarios(int numeroFuncionarios) {
+		this.numeroFuncionarios = numeroFuncionarios;
+	}
+
+	@Override
+	public double calcularImposto() {
+		double total = 0.0;
+		
+		if(numeroFuncionarios < 10) {
+			total = super.getRendaAnual() * 0.16;
+		}else if(numeroFuncionarios >= 10) {
+			total = super.getRendaAnual() * 0.14;
+		}
+		return total;
+	}
 	
 	
 
